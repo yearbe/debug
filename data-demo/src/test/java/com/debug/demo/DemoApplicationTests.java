d9001f92c299d88b6bf4c897862315d942f47873
yearbe <1658539213@qq.com>
2020-01-21 11:48:17 +0800

    test

--- data-demo/src/test/java/com/debug/demo/DemoApplicationTests.java
+++ data-demo/src/test/java/com/debug/demo/DemoApplicationTests.java
@@ -9,26 +9,29 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 //@SpringBootTest
 class DemoApplicationTests {
     @Test
-    void contextLoads() {
+    void contextLoads() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(100);
+        Future<User> future;
         for (int i = 0; i < 1000; i++) {
-            executorService.execute(() -> {
+            future = executorService.submit(() -> {
                 WebClient webClient = WebClient.create("http://localhost:7878/user");
                 Mono<User> user = webClient.get().uri("/1")
                         .accept(MediaType.APPLICATION_JSON)
                         .header("token", "1")
                         .exchange()
                         .flatMap(res -> res.bodyToMono(User.class));
-                System.out.println(user.block());
+                return user.block();
             });
+            System.out.println(future.get());
         }
-        while (true) {}
     }
 
 }
