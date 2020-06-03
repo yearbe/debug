e7ce31577af424314dd2565f25ce555fb6922402
yearbe <1658539213@qq.com>
2020-06-03 17:01:56 +0800

    upload

---
+++ spring-cloud-cluster/client/src/main/java/com/debug/client/service/PhoneService.java
@@ -0,0 +1,4 @@
+package com.debug.client.service;
+
+public interface PhoneService {
+}
---
+++ spring-cloud-cluster/client/src/main/java/com/debug/client/service/impl/PhoneServiceImpl.java
@@ -0,0 +1,11 @@
+package com.debug.client.service.impl;
+
+/**
+ * @author Lyb
+ * @since 2020-06-03
+ */
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneServceImpl  {
+}
